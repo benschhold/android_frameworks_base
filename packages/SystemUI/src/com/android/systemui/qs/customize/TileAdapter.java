a5e5f2996180192388066d708ce93bb6f3fa0e87
Ivan Iskandar <iiiiskandar14@gmail.com>
2016-09-18 21:33:18 +0700

    SystemUI: Expose QS edit item decoration background color
    PS2:
    More descriptive color name
    Change-Id: I9867ca26e7d9dacad37d2b70180a98fede0fb0e8

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -471,7 +471,7 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
     private final OmniSpanSizeLookup mSizeLookup = new OmniSpanSizeLookup();
 
     private class TileItemDecoration extends ItemDecoration {
-        private final ColorDrawable mDrawable;
+        private ColorDrawable mDrawable = new ColorDrawable();
 
         private TileItemDecoration(Context context) {
             TypedArray ta =
